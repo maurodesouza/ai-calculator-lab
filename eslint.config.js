fe8c7eeac4e7441bc71c75609210f283d0516d4b
Mauro de Souza <maurodesouza.me@gmail.com>
2026-06-20 19:06:15 -0300

    🧼 lint: set up eslint and prettier
    Add ESLint flat config with TypeScript, React, and import plugins.
    Enforce function declarations, named exports, type over interface,
    and prettier formatting. Add format and lint scripts.
    
    Refs #6

---
+++ eslint.config.js
@@ -0,0 +1,77 @@
+import js from '@eslint/js'
+import prettier from 'eslint-config-prettier'
+import importPlugin from 'eslint-plugin-import'
+import prettierPlugin from 'eslint-plugin-prettier'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  {
+    ignores: ['dist/**', 'dist-ssr/**', '.tanstack/**', 'node_modules/**', 'src/routeTree.gen.ts'],
+  },
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  react.configs.flat.recommended,
+  react.configs.flat['jsx-runtime'],
+  importPlugin.flatConfigs.recommended,
+  {
+    plugins: {
+      'react-hooks': reactHooks,
+      prettier: prettierPlugin,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        console: 'readonly',
+        document: 'readonly',
+        window: 'readonly',
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      ...prettier.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/function-component-definition': [
+        'error',
+        {
+          namedComponents: 'function-declaration',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
+      'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      'import/no-default-export': 'error',
+      'import/no-unresolved': 'off',
+      'import/named': 'off',
+    },
+  },
+  {
+    files: ['src/router.tsx', '**/*.d.ts'],
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': 'off',
+    },
+  },
+  {
+    files: ['vite.config.ts', 'eslint.config.js'],
+    rules: {
+      'import/no-default-export': 'off',
+    },
+  }
+)
